2cffbebf341229534ba23616d4effb8fa63e4dd8
Taylor Romero <me@taylorrome.ro>
2020-12-18 08:38:35 -0700

    patch: type testing

--- src/event.types.ts
+++ src/event.types.ts
@@ -1,19 +1,33 @@
+/* eslint-disable @typescript-eslint/ban-types */
 import { EventContract, MercuryEventEmitter } from '@sprucelabs/mercury-types'
 import { SpruceSchemas } from '@sprucelabs/spruce-core-schemas'
 
 type Skill = SpruceSchemas.Spruce.v2020_07_22.Skill
 
-export type SpruceEvent<
-	EventContracts extends EventContract,
-	EmitPayload extends Record<string, any> | undefined = undefined
-> = {
-	skill: Skill
-	apiClient: MercuryEventEmitter<EventContracts>
-} & (EmitPayload extends Record<string, any>
+type OptionalApiClient<
+	IEventContract extends EventContract | undefined
+> = IEventContract extends EventContract
+	? {
+			apiClient: MercuryEventEmitter<IEventContract>
+	  }
+	: {}
+
+type OptionalPayload<
+	EmitPayload extends Record<string, any> | undefined
+> = EmitPayload extends Record<string, any>
 	? {
 			payload: EmitPayload
 	  }
-	: // eslint-disable-next-line @typescript-eslint/ban-types
-	  {})
+	: {}
 
-export type SpruceEventResponse<ResponsePayload> = Promise<ResponsePayload>
+export type SpruceEvent<
+	IEventContract extends EventContract | undefined = undefined,
+	EmitPayload extends Record<string, any> | undefined = undefined
+> = {
+	skill: Skill
+} & OptionalApiClient<IEventContract> &
+	OptionalPayload<EmitPayload>
+
+export type SpruceEventResponse<
+	ResponsePayload extends any = undefined
+> = Promise<ResponsePayload>
