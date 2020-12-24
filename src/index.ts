94a9181c0481aa5bc02aa5fbefec0df62b04234c
Taylor Romero <me@taylorrome.ro>
2020-12-23 17:46:25 -0700

    patch: missed export

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export { default as eventContractUtil } from './utilities/eventContract.utility'
 export { default as eventErrorAssertUtil } from './utilities/eventErrorAssert.utility'
 export { default as eventResponseUtil } from './utilities/eventResponse.utility'
+export { default as eventDiskUtil } from './utilities/eventDiskUtil.utility'
 export { default as buildEmitTargetAndPayloadSchema } from './utilities/buildEmitTargetAndPayloadSchema'
 export * from './utilities/buildEmitTargetAndPayloadSchema'
 export * from './event.types'
