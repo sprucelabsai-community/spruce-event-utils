0af76c02fda3161e346bf814a08bb8d59e9684ef
Taylor Romero <me@taylorrome.ro>
2020-12-23 17:32:25 -0700

    patch: event disk utils cleanup

--- src/__tests__/behavioral/SplittingPathsIntoEvent.test.ts
+++ src/__tests__/behavioral/SplittingPathsIntoEvent.test.ts
@@ -8,30 +8,134 @@ export default class SplittingPathsIntoEventTest extends AbstractSpruceTest {
 	}
 
 	@test()
+	protected static throwsIfNotListener() {
+		assert.doesThrow(() =>
+			eventDiskUtil.splitPathToListener(
+				this.resolvePath('src/listeners/my-great-event/test-event.listener.ts')
+			)
+		)
+
+		assert.doesThrow(() =>
+			eventDiskUtil.splitPathToListener(
+				this.resolvePath('my-great-event/test-event.listener.ts')
+			)
+		)
+
+		assert.doesThrow(() =>
+			eventDiskUtil.splitPathToListener(
+				this.resolvePath('my-great-event/v2020_02_02.listener.ts')
+			)
+		)
+	}
+
+	@test()
+	protected static throwsIfNotEvent() {
+		assert.doesThrow(() =>
+			eventDiskUtil.splitPathToListener(
+				this.resolvePath('src/events/my-great-event/emitPayload.builder.ts')
+			)
+		)
+
+		assert.doesThrow(() =>
+			eventDiskUtil.splitPathToListener(
+				this.resolvePath('src/events/v2020_02_02/emitPayload.builder.ts')
+			)
+		)
+	}
+
+	@test()
 	protected static canEventPath() {
 		const eventPath = this.resolvePath(
-			'/src/events/v2020_10_10/my-great-event/emitPayload.builder.ts'
+			'src/events/my-great-event/v2020_10_10/emitPayload.builder.ts'
 		)
 
 		assert.isEqualDeep(eventDiskUtil.splitPathToEvent(eventPath), {
-			isEvent: true,
 			eventName: 'my-great-event',
 			version: 'v2020_10_10',
 		})
 	}
 
 	@test()
-	protected static canEventListener() {
+	protected static canListener() {
 		const eventPath = this.resolvePath(
-			'/src/listeners/some-great-skill/v2020_10_10/did-book.listener.ts'
+			'src/listeners/some-great-skill/v2020_10_10/did-book.listener.ts'
 		)
 
-		assert.isEqualDeep(eventDiskUtil.splitPathToEvent(eventPath), {
-			isListener: true,
+		assert.isEqualDeep(eventDiskUtil.splitPathToListener(eventPath), {
 			eventNamespace: 'some-great-skill',
 			eventName: 'did-book',
 			eventNameWithOptionalNamespace: 'some-great-skill.did-book',
 			version: 'v2020_10_10',
 		})
+	}
+
+	@test()
+	protected static throwsWhenResolvingBadListener() {
+		assert.doesThrow(() =>
+			//@ts-ignore
+			eventDiskUtil.resolveListenerPath(this.cwd, {
+				eventName: 'my-event',
+				eventNamespace: 'test',
+			})
+		)
+		assert.doesThrow(() =>
+			//@ts-ignore
+			eventDiskUtil.resolveListenerPath(this.cwd, {
+				eventName: 'my-event',
+				version: '123',
+			})
+		)
+
+		assert.doesThrow(() =>
+			//@ts-ignore
+			eventDiskUtil.resolveListenerPath(this.cwd, {
+				eventNamespace: 'test',
+				version: '123',
+			})
+		)
+	}
+
+	@test()
+	protected static throwsWhenResolvingBadEvent() {
+		assert.doesThrow(() =>
+			//@ts-ignore
+			eventDiskUtil.resolveEventPath(this.cwd, {
+				eventName: 'my-event',
+			})
+		)
+
+		assert.doesThrow(() =>
+			//@ts-ignore
+			eventDiskUtil.resolveEventPath(this.cwd, {
+				version: '123',
+			})
+		)
+	}
+
+	@test()
+	protected static resolvesListenerAsExpected() {
+		const e = {
+			eventName: 'did-book',
+			eventNamespace: 'booking',
+			version: 'v2020_02_02',
+		}
+
+		const expected = this.resolvePath(
+			`booking/v2020_02_02/did-book.listener.ts`
+		)
+
+		assert.isEqual(eventDiskUtil.resolveListenerPath(this.cwd, e), expected)
+	}
+
+	@test()
+	protected static resolvesEventAsExpected() {
+		const e = {
+			eventName: 'did-book',
+			version: 'v2020_02_02',
+		}
+
+		const expected = this.resolvePath(`did-book/v2020_02_02`)
+
+		assert.isEqual(eventDiskUtil.resolveEventPath(this.cwd, e), expected)
 	}
 }
--- src/utilities/eventDiskUtil.utility.ts
+++ src/utilities/eventDiskUtil.utility.ts
@@ -2,7 +2,6 @@ import eventContractUtil from './eventContract.utility'
 
 interface Event {
 	eventName: string
-	isEvent: true
 	version: string
 }
 
@@ -10,36 +9,84 @@ interface Listener {
 	eventName: string
 	eventNamespace: string
 	eventNameWithOptionalNamespace: string
-	isListener: true
 	version: string
 }
 
 const eventDiskUtil = {
-	splitPathToEvent(match: string, seperator = '/'): Event | Listener {
-		const matchParts = match.split(seperator)
-		const isEvent = match.search(`${seperator}events${seperator}`) > -1
-
-		const event: any = {}
-
-		if (isEvent) {
-			matchParts.pop()
-			event.isEvent = true
-			event.eventName = matchParts.pop() ?? ''
-			event.version = matchParts.pop() as string
-		} else {
-			event.isListener = true
-			event.eventName = matchParts.pop()?.split('.')[0] as string
-			event.version = matchParts.pop() as string
-			event.eventNamespace = matchParts.pop() as string
-			event.eventNameWithOptionalNamespace = eventContractUtil.joinEventNameWithOptionalNamespace(
-				{
-					eventName: event.eventName,
-					eventNamespace: event.eventNamespace,
-				}
+	resolveListenerPath(
+		destination: string,
+		e: { eventNamespace: string; eventName: string; version: string },
+		seperator = '/'
+	) {
+		if (!e || !e.eventName || !e.eventNamespace || !e.version) {
+			throw new Error(
+				"Can't resolve path to listener without eventName, eventNamespace, version"
 			)
 		}
 
+		return [
+			destination,
+			e.eventNamespace,
+			e.version,
+			e.eventName + '.listener.ts',
+		].join(seperator)
+	},
+
+	splitPathToListener(match: string, seperator = '/'): Listener {
+		const matches = new RegExp(
+			`([a-z\\-]*)\\${seperator}(v[0-9]{4}_[0-9]{2}_[0-9]{2})\\${seperator}([a-z\\-]*)\\.listener\\.ts`
+		).exec(match)
+
+		if (!matches) {
+			throw new Error(
+				"Invalid listener path. Must be in a folder structure like '**/src/listeners/{{eventNamespace}}/{{version}}/{{eventName}}.listener.ts"
+			)
+		}
+
+		const listener: any = {}
+
+		listener.eventNamespace = matches[1]
+		listener.version = matches[2]
+		listener.eventName = matches[3]
+		listener.eventNameWithOptionalNamespace = eventContractUtil.joinEventNameWithOptionalNamespace(
+			{
+				eventName: listener.eventName,
+				eventNamespace: listener.eventNamespace,
+			}
+		)
+
+		return listener
+	},
+	splitPathToEvent(match: string, seperator = '/'): Event {
+		const matches = new RegExp(
+			`([a-z\\-]*)\\${seperator}(v[0-9]{4}_[0-9]{2}_[0-9]{2})\\${seperator}.*?\\.builder\\.ts`
+		).exec(match)
+
+		if (!matches) {
+			throw new Error(
+				"Invalid event path. Must be in a folder structure like '**/src/events/{{eventName}}/{{version}}/emitPayloadSchema.builder.ts"
+			)
+		}
+
+		const event: any = {}
+
+		event.eventName = matches[1]
+		event.version = matches[2]
+
 		return event
+	},
+	resolveEventPath(
+		destination: string,
+		e: { eventName: string; version: string },
+		seperator = '/'
+	) {
+		if (!e || !e.eventName || !e.version) {
+			throw new Error(
+				"Can't resolve path to event without eventName, eventNamespace, version"
+			)
+		}
+		e
+		return [destination, e.eventName, e.version].join(seperator)
 	},
 }
 
