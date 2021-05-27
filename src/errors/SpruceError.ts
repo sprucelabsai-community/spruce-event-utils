f48bb6b89419233ccf47ed2d56439839f8ff74c4
Taylor Romero <me@taylorrome.ro>
2021-05-27 09:25:37 -0600

    patch: error improevement

--- src/errors/SpruceError.ts
+++ src/errors/SpruceError.ts
@@ -28,7 +28,7 @@ export default class SpruceError extends BaseSpruceError<ErrorOptions> {
 			}
 
 			case 'EMPTY_MERCURY_RESPONSE':
-				return 'Got no results back from the server! That was unexpected.'
+				return "Got no results back from the server! That was unexpected. It may be because the event you're listening to does not have a response payload."
 
 			case 'DUPLICATE_EVENT':
 				message = `'${options.fullyQualifiedEventName}' event already exists.`
