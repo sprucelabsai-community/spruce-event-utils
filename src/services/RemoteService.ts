0419ab861ac85d44ba61c7ba0c75f5ca82752863
Tay <me@taylorrome.ro>
2021-07-21 21:58:04 -0600

    minor: remote service

---
+++ src/services/RemoteService.ts
@@ -0,0 +1,28 @@
+import { EnvService } from '@sprucelabs/spruce-skill-utils'
+import { Remote, REMOTES } from '../constants'
+
+//extracted from cli, local testing going forward
+export default class RemoteService {
+	private env: EnvService
+
+	public constructor(env: EnvService) {
+		this.env = env
+	}
+
+	public set(remote: Remote) {
+		const host = REMOTES[remote]
+		this.env.set('HOST', host)
+	}
+
+	public getHost() {
+		return this.env.get('HOST')
+	}
+
+	public getRemote() {
+		// move to constants or some better mapping?
+		const values = Object.entries(REMOTES)
+		const host = this.getHost()
+		const match = values.find((v) => v[1] === host)
+		return match?.[0] as Remote
+	}
+}
