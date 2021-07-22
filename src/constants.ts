3d04f2cb5497d03e9b9eac107a5b807236a5c3ea
Tay <me@taylorrome.ro>
2021-07-21 19:19:36 -0600

    minor: remote utils and remotes

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,15 @@
 export const EVENT_VERSION_DIVIDER = '::'
+
+export const REMOTE_LOCAL = 'http://127.0.0.1:8081'
+export const REMOTE_DEV = 'https://developer.mercury.spruce.ai'
+export const REMOTE_SANDBOX = 'https://sandbox.mercury.spruce.ai'
+export const REMOTE_PROD = 'https://mercury.spruce.ai'
+
+export const REMOTES = {
+	local: REMOTE_LOCAL,
+	dev: REMOTE_DEV,
+	sandbox: REMOTE_SANDBOX,
+	prod: REMOTE_PROD,
+}
+
+export type Remote = keyof typeof REMOTES
---
+++ src/utilities/heartwoodRemote.utility.ts
@@ -0,0 +1,23 @@
+import { SpruceError } from '@sprucelabs/schema'
+import { Remote, REMOTES } from '../constants'
+
+export const HEARTWOOD_REMOTES: Record<Remote, string> = {
+	local: 'http://localhost:6006',
+	dev: 'https://dev.spruce.bot',
+	sandbox: 'https://sandbox.spruce.bot',
+	prod: 'https://spruce.bot',
+}
+export const heartwoodRemoteUtil = {
+	buildViewWatchUrl(remote: Remote) {
+		if (HEARTWOOD_REMOTES[remote]) {
+			return HEARTWOOD_REMOTES[remote]
+		}
+		throw new SpruceError({
+			code: 'INVALID_PARAMETERS',
+			friendlyMessage: `\`${remote}\` is not a valid Remote. Valid options are:\n\n${Object.keys(
+				REMOTES
+			).join('\n')}`,
+			parameters: ['remote'],
+		})
+	},
+}
