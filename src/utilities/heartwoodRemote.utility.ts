f728caf71bbad943aa98ed7409f4c7ab56450e45
Ryan Johnson <ryan@sprucelabs.ai>
2022-02-11 11:57:09 -0500

    patch: updated HEARTWOOD_REMOTES to have dev.spruce.bot for developer env

--- src/utilities/heartwoodRemote.utility.ts
+++ src/utilities/heartwoodRemote.utility.ts
@@ -3,7 +3,7 @@ import { Remote, REMOTES } from '../constants'
 
 export const HEARTWOOD_REMOTES: Record<Remote, string> = {
 	local: 'http://localhost:6006',
-	developer: 'https://developer.spruce.bot',
+	developer: 'https://dev.spruce.bot',
 	sandbox: 'https://sandbox.spruce.bot',
 	production: 'https://spruce.bot',
 	heartwood_test: 'https://heartwood-test.spruce.bot',
