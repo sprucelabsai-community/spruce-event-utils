d2f62ad14b8a4cf13cb01dc6f5a1e03fa433ea70
Taylor Romero <me@taylorrome.ro>
2022-03-03 14:01:13 -0700

    patch: missed export settings

--- src/schemas/v2021_09_13/eventPaging.builder.ts
+++ src/schemas/v2021_09_13/eventPaging.builder.ts
@@ -3,6 +3,8 @@ import { buildSchema } from '@sprucelabs/schema'
 export default buildSchema({
 	id: 'eventPaging',
 	name: 'Event Paging',
+	moduleToImportFromWhenRemote: '@sprucelabs/spruce-event-utils',
+	importsWhenRemote: ["import '@sprucelabs/spruce-event-utils'"],
 	fields: {
 		pageSize: {
 			type: 'number',
--- src/schemas/v2021_09_13/eventTarget.builder.ts
+++ src/schemas/v2021_09_13/eventTarget.builder.ts
@@ -4,8 +4,8 @@ import { messageTargetSchema } from '@sprucelabs/spruce-core-schemas'
 export default buildSchema({
 	id: 'eventTarget',
 	name: 'event target',
-	importsWhenRemote: ["import '@sprucelabs/spruce-event-utils'"],
 	moduleToImportFromWhenRemote: '@sprucelabs/spruce-event-utils',
+	importsWhenRemote: ["import '@sprucelabs/spruce-event-utils'"],
 	fields: {
 		...dropFields(messageTargetSchema.fields, ['phone']),
 	},
